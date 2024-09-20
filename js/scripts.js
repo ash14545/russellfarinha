8f6d1ad1817eb567b434801fa3f445971cbac00e
Russell <russellangfarinha@gmail.com>
2024-09-20 17:37:49 -0500

    responsive to 320px

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,38 @@
-// Add any JavaScript you need for interactivity here
+// Sticky top navbar
+function stickyNav(elemLoc) {
+    if(elemLoc >= $(window).scrollTop()) {
+        if($('.navbar').hasClass('fixed-navbar')) {
+            $('.navbar').removeClass('fixed-navbar');
+        }
+    } else { 
+        if(!$('.navbar').hasClass('fixed-navbar')) {
+            $('.navbar').addClass('fixed-navbar');
+        }
+    }
+}
+
+$(document).ready(function() {
+    var elemLoc = $('.navbar').position().top;
+    stickyNav(elemLoc);
+    $(window).scroll(function() {
+        stickyNav(elemLoc);
+    });
+});
+
+$(document).ready(function() {
+    $('a[href^="#"]').on('click', function(event) {
+        event.preventDefault();
+
+        var target = $(this.getAttribute('href'));
+        if (target.length) {
+            var remValue = 3; // Adjust this value to your desired rem value
+            var rootFontSize = parseFloat(getComputedStyle(document.documentElement).fontSize);
+            var offset = remValue * rootFontSize;
+
+            var targetPosition = target.offset().top - offset;
+            $('html, body').animate({
+                scrollTop: targetPosition
+            }, 1000); // Adjust the duration as needed
+        }
+    });
+});
